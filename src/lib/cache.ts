99ef1c389bff0181daeacb8d5c0ed48913e7b53b
2547techno <technowizz2547@gmail.com>
2023-09-01 17:46:58 -0400

    fetch all bid items on startup

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,5 +1,19 @@
+import { config } from "./config.js";
+import { db } from "./db.js";
+
 export const cache: Map<string, any> = new Map();
 
-export function initCache() {
+export async function initCache() {
     cache.set("currentBidItemId", 1);
+
+    const rows = await db.pool.query(
+        `SELECT * FROM ${config.db.database}.ITEMS`
+    );
+    const items: {
+        [id: number]: string;
+    } = {};
+    for (const row of rows) {
+        items[row.id] = row.name;
+    }
+    cache.set("bidItems", items);
 }
